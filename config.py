b0ab263c7e6538b43dce7001aeb1925d8f8d08bd
ali0025 <ec21183@glbitm.ac.in>
2025-03-25 02:13:38 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -2,5 +2,11 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")  # Default to localhost if not set
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
